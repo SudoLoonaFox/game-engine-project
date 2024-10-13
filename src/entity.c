3f6df2e8374d8370fc9deefa98beaaa039c25396
SudoLoonaFox <sudoloonafox@gmail.com>
2024-10-14 12:41:26 +1300

    fix: Correct spelling errors

--- src/entity.c
+++ src/entity.c
@@ -8,7 +8,7 @@
 Observers can be entities, achievements, quests or environment
 An example use is rain starts and npcs seek cover
 Another example is you kill 10 enemies with x weapon and get an achievement
-Observers need to run code on recieving an update from an entity
+Observers need to run code on receiving an update from an entity
 Observers will use a generic type with pointers to update specific objects
 The path an event may go is entity/environment/npc/enemy->observer->entity/quest/
 Observers also need to be able to read data of the objects they are observing
@@ -17,11 +17,11 @@ A quest may get an event for an enemy dying. The quest needs to check the enemy 
 
 /*
 Entities need to either store component data or have component data referenced by an id
-Entites need to be able to notify observers of an event taken
+Entities need to be able to notify observers of an event taken
 */
 
 /*
-Entites may be initalized in an array but most access will be by refernce
+Entities may be initialized in an array but most access will be by reference
 Should I include the register unregister functions in the struct?
 Reg, unreg, notify, destroy can be moved out of this; however,
 I am keeping them in for now so functions they are passed to can call their functions
--- src/observer.c
+++ src/observer.c
@@ -7,7 +7,7 @@
 Observers can be entities, achievements, quests or environment
 An example use is rain starts and npcs seek cover
 Another example is you kill 10 enemies with x weapon and get an achievement
-Observers need to run code on recieving an update from an entity
+Observers need to run code on receiving an update from an entity
 Observers will use a generic type with pointers to update specific objects
 The path an event may go is entity/environment/npc/enemy->observer->entity/quest/
 Observers also need to be able to read data of the objects they are observing
@@ -16,7 +16,7 @@ A quest may get an event for an enemy dying. The quest needs to check the enemy 
 
 /*
 Entities need to either store component data or have component data referenced by an id
-Entites need to be able to notify observers of an event taken
+Entities need to be able to notify observers of an event taken
 */
 
 // Observers can be attached to different types of objects. Some will be generic and others will be specific
